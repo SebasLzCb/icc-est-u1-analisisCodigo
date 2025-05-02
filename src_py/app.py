0e431452504282e205675b6bcb6c1bb7a9c87622
jsanchezr6 <jsanchezr6@est.ups.edu.ec>
2025-05-01 19:47:59 -0500

    Implemnetacion de los metodos faltantes

--- src_py/app.py
+++ src_py/app.py
@@ -1,7 +1,34 @@
 #import benchmarking as bm
-from benchmarking import benchmarking
+import benchmarking as bm
+from metodo_ordenamiento import MetodosOrdenamiento
 # Archivo principal o main
 if __name__ == "__main__":
     print("Funciona")
-    # bm.benchmarking()
-    benchmarking()
+    bench = bm.benchmarking()
+    metodosO = MetodosOrdenamiento()
+    
+    #tam = 10000
+    tamanios = [5000, 10000, 20000]
+    
+    resultados = []
+    
+    for tam in tamanios:
+        
+        arreglo_base = bench.build_arreglo(tam)
+    
+        metodos_dic = {
+            "burbuja" : metodosO.sort_bubble,
+            "seleccion" : metodosO.sort_seleccion,
+            "burbujamejorado" : metodosO.sort_burbuja_mejorado_optimizado,
+            "shell" : metodosO.sort_shells
+        }
+        
+        for nombre, fun_metodo in metodos_dic.items():
+            
+            tiempo_resultado = bench.medir_tiempo(fun_metodo, arreglo_base)
+            tupla_resultado = (tam, nombre, tiempo_resultado)
+            resultados.append(tupla_resultado)
+            
+    for tam, nombre, tiempo_resultado in resultados:
+        print(f'Tamaño: {tam}, Nombre metodo: {nombre}, Tiempo: {tiempo_resultado:.6f} segundos')
+        
--- src_py/benchmarking.py
+++ src_py/benchmarking.py
@@ -6,37 +6,45 @@ class benchmarking:
     # public Benchmarking() {
     def __init__(self):
         print('Bechmarking instaciado')
+        
+    
+    def medir_tiempo(self, funcion, arreglo):
+        
+        inicio = time.perf_counter()
+        funcion(arreglo)
+        fin = time.perf_counter()
+        return fin - inicio
 
-        self.mO = MetodosOrdenamiento()
-        arreglo = self.build_arreglo(10000)
+        # self.mO = MetodosOrdenamiento()
+        # arreglo = self.build_arreglo(10000)
 
-        tarea = lambda: self.mO.sort_bubble(arreglo)
-        tarea2 = lambda: self.mO.sort_seleccion(arreglo)
-        tarea3 = lambda: self.mO.sort_burbuja_mejorado_optimizado(arreglo)
-        tarea4 = lambda: self.mO.sort_shells(arreglo)
+        #tarea = lambda: self.mO.sort_bubble(arreglo)
+        #tarea2 = lambda: self.mO.sort_seleccion(arreglo)
+        #tarea3 = lambda: self.mO.sort_burbuja_mejorado_optimizado(arreglo)
+        #tarea4 = lambda: self.mO.sort_shells(arreglo)
 
         #tiempomilles = self.contar_con_current_time_milles(tarea)
 
-        tiemponano = self.contar_con_nano_time(tarea)
-        tiemponano2 = self.contar_con_nano_time(tarea2)
-        tiemponano3 = self.contar_con_nano_time(tarea3)
-        tiemponano4 = self.contar_con_nano_time(tarea4)
+        #tiemponano = self.contar_con_nano_time(tarea)
+        #tiemponano2 = self.contar_con_nano_time(tarea2)
+        #tiemponano3 = self.contar_con_nano_time(tarea3)
+        #tiemponano4 = self.contar_con_nano_time(tarea4)
 
-        print("Este es el metodo de ordenamiento burbuja")
-        print(f"Tiempo con nanoTime: {tiemponano} ")
-        print(" ")
+        #print("Este es el metodo de ordenamiento burbuja")
+        #print(f"Tiempo con nanoTime: {tiemponano} ")
+        #print(" ")
 
-        print("Este es el metodo de ordenamiento burbuja mejorado optimizado")
-        print(f"Tiempo con nanoTime: {tiemponano3} ")
-        print(" ")
+        #print("Este es el metodo de ordenamiento burbuja mejorado optimizado")
+        #print(f"Tiempo con nanoTime: {tiemponano3} ")
+        #print(" ")
 
-        print("Este es el metodo de ordenamiento seleccion")
-        print(f"Tiempo con nanoTime: {tiemponano2} ")
-        print(" ")
+        #print("Este es el metodo de ordenamiento seleccion")
+        #print(f"Tiempo con nanoTime: {tiemponano2} ")
+        #print(" ")
 
-        print("Este es el metodo de ordenamiento shell")
-        print(f"Tiempo con nanoTime: {tiemponano4} ")
-        print(" ")
+        #print("Este es el metodo de ordenamiento shell")
+        #print(f"Tiempo con nanoTime: {tiemponano4} ")
+        #print(" ")
 
 
     def build_arreglo(self, tamano):
